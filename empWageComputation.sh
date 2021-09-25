e352ea558dcb1b602db7dc9cb805980d465e1c37
[somilpatil] <[patilsomil118@gmail.com]>
2021-09-25 18:45:51 +0530

    [somilpatil]added:Empwagecalculate

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,14 +5,18 @@ echo "welcome to the EmpWage problem"
 empcheck=$((RANDOM%2)) # 0/1
 #constant variable
 IS_PRESENT=1
+emprate=20
+emphrs=8
 
 #selection
 if [ $empcheck -eq $IS_PRESENT ]]
 then
-
+    salary=$((empHrs*emprate))
      echo "emp is present"
 else
+     salary=0
      echo "emp is absent"
 fi
+
 
 
