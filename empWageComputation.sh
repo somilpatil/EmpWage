004c86874710dff6c868bd5d5e34f17b518462f7
[somilpatil] <[patilsomil118@gmail.com]>
2021-09-25 18:26:59 +0530

    [somilpatil]added:EmpAttendance feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,18 @@
 #! bin/bash -x
 
 echo "welcome to the EmpWage problem"
+#variable
+empcheck=$((RANDOM%2)) # 0/1
+#constant variable
+IS_PRESENT=1
+
+#selection
+if [ $empcheck -eq $IS_PRESENT ]]
+then
+
+     echo "emp is present"
+else
+     echo "emp is absent"
+fi
+
 
